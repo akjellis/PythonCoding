77d87fc4e1de7f9b8c09ae695b7f9064158cbcf0
Andy Ellis <akjellis1@yahoo.com>
2020-12-06 22:08:53 -0800

    added files

---
+++ Day 2 Data Types/Primitive Data Types.py
@@ -0,0 +1,21 @@
+
+#print("Hello"[4])
+# This will tell me what the 4th character is. Remember counting starts at 0 not 1.
+
+#**********************************************************
+#Code below shows how to add a two digit number together
+
+two_digit_number = input("Type a two digit number: ")
+
+#print (type("two_digit_number")) This tells me data type
+
+first_number = int(two_digit_number[0])
+second_number = int(two_digit_number[1])
+
+#print (type("first_number"))
+
+result = (first_number + second_number)
+
+print(result)
+
+#********************************************************************
